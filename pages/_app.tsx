3b841e68e8e130b51d965e690e03d2f75f328410
akihide.egashira <42921740+EGASHIRAAkihide@users.noreply.github.com>
2022-10-22 19:25:56 +0900

    (feat) [issue-016] home, about作成

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>ENEN inc</title>
+        <meta name="description" content="This is ENEN-inc website." />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
 
 export default MyApp;
---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function About() {
+  return (
+    <div>
+      About
+      <Link href="/">home</Link>
+    </div>
+  );
+}
+
+export default About;
